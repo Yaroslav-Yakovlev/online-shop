883ca450314ce730b62b1aed4c4165a1dba65915
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2023-07-27 12:26:43 +0300

    removed from git .env file, added the axios package, created createOrUpdateUser function for POST request

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,8 +1,25 @@
 import {Request, Response} from "express";
+import User, {IUser} from "../models/user";
 
-export const createOrUpdateUser = (req: Request, res: Response) => {
-    res.json({
-        data: 'hey you hit create-or-update-user API endpoint',
-    });
-}
+export type AuthenticatedRequest = Request & { user: IUser };
 
+export const createOrUpdateUser = async (req: AuthenticatedRequest, res: Response) => {
+    const {name, email, picture} = req.user;
+
+    const user = await User.findOneAndUpdate(
+        {email},
+        {name, picture},
+        {new: true},
+    );
+    if (user) {
+        console.log('user updated', user)
+        res.json(user);
+    } else {
+        const newUser = await new User({
+            email,
+            name,
+            picture,
+        }).save();
+        console.log('user created', newUser)
+    }
+};
